58218ec2b5c42df7caeb8bcb369e43873f7cc136
Гайнутдинова Алина Булатовна <211908@mck-ktits.ru>
2021-07-01 08:39:32 +0300

    Добавьте файлы проекта.

---
+++ MonitorTemp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonitorTemp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var value1 = Convert.ToInt32(textBox1.Text);
+            var value2 = Convert.ToInt32(textBox2.Text);
+
+            var x = value1 + value2 ;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
